5074ec00a320f86bbd1e06ecf0b189d32f506596
piterwifraid-ops <piterwifraid@gmail.com>
2026-02-20 02:55:12 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Checkout from "./pages/Checkout";
+import CheckoutAddress from "./pages/CheckoutAddress";
+import CheckoutPayment from "./pages/CheckoutPayment";
+import PixPaymentScreen from "./pages/PixPaymentScreen";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +21,10 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Checkout/address" element={<CheckoutAddress />} />
+          <Route path="/Checkout/address/payment" element={<CheckoutPayment />} />
+          <Route path="/Checkout/address/payment/pix" element={<PixPaymentScreen />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
